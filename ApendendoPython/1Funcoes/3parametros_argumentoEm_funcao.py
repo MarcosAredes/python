e765dfef8dec6fc9dcee2e1f7c6d2865525c1d4b
MarcosAredes <Ducarx40@hotmail.com>
2023-11-30 15:07:42 -0300

    Argumentação

---
+++ ApendendoPython/1Funcoes/3parametros_argumentoEm_funcao.py
@@ -0,0 +1,44 @@
+
+
+
+
+
+             # Esse e o paramentro
+def boas_vindas(nome, quantidade):
+    print(f'Olá {nome}.')
+    print(f'Temos {str(quantidade)} notebooks em estoque')
+
+boas_vindas('Mustang', 5)
+boas_vindas('Camaro', 3)
+boas_vindas('plymouth', 1)
+            #Aqui são os argumentos
+
+
+
+
+
+
+
+
+
+'''
+def lojinha():
+    print('Olá Mustang !')
+    print('Temos 5 notebooks em estoque')
+
+lojinha()
+
+
+def lojinha2():
+    print('Olá Camaro!')
+    print('Temos 3 notebooks em estoque')
+
+lojinha2()
+
+
+def lojinha3():
+    print('Olá plymouth !')
+    print('Temos 1 notebooks em estoque')
+
+lojinha3()
+'''
