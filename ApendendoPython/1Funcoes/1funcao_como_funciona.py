03873dc596f28068067247fe5ada0c3754397842
MarcosAredes <Ducarx40@hotmail.com>
2023-11-30 01:02:43 -0300

    funçãopt2

---
+++ ApendendoPython/1Funcoes/1funcao_como_funciona.py
@@ -0,0 +1,27 @@
+
+
+
+
+def lojinha():
+    print('Olá Mustang !')
+    print('Temos 5 notebooks em estoque')
+
+lojinha()
+
+#Tanto de código
+#Tanto de código
+#Tanto de código
+#Tanto de código
+#Tanto de código
+#Tanto de código
+
+lojinha()
+
+'''
+Resultado:
+Olá Mustang !
+Temos 5 notebooks em estoque
+Olá Mustang !
+Temos 5 notebooks em estoque
+'''
+
