18080c4fd878495ac6406eccd5d85df5a4f0c301
MarcosAredes <Ducarx40@hotmail.com>
2023-11-30 16:24:39 -0300

    Atualização

---
+++ ApendendoPython/1Funcoes/4Argumentos_Default_Nondefault.py
@@ -0,0 +1,28 @@
+
+
+
+            #  O parametro quantidade e o Default pois o valor foi definido no parametro
+def lojinha(nome, quantidade=9):
+    print(f'Olá {nome} !')
+    print(f'Temos {str(quantidade)} notebooks em estoque')
+
+lojinha('otario') 
+
+'''
+
+Default pode ser alterado dentro do Non Default 
+
+But, mas, pqp
+
+Default e Non Default tem q seguir a ordem
+
+1° Non Default e depois --> Non Default
+
+Non Default --> Non Default
+
+No codigo seria
+
+lojinha('O nome') e depois como quantidade e = default vem depois pois já esta definido 
+
+
+'''
---
+++ ApendendoPython/1Funcoes/5Print_Return_em_Funções.py
@@ -0,0 +1,15 @@
+
+
+
+def client1(nome):
+    print(f'O que o senhor {nome} gostaria')
+
+client1('Cadabra')
+
+
+
+
+def client2(nome):
+    return f'Olá {nome}'
+
+print(client2('AbraCadabra'))
