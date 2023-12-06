631d0ff217a94dcd35890f30d44334f9625e8d44
MarcosAredes <Ducarx40@hotmail.com>
2023-12-06 18:59:59 -0300

    Mais coisas

---
+++ ApendendoPython/2Estrutura_de_Dados/4Concatenando_listas.py
@@ -0,0 +1,29 @@
+
+
+'''numbers = [1, 2, 3, 4, 5]
+
+final = numbers * 2
+print(final)'''
+
+#----------------------------------------------
+
+'''numbers = [1, 2, 3, 4, 5]
+letras = ['a' , 'b' , 'c' , 'd' , 'e']
+
+final = numbers + letras
+print(final)'''
+
+#-----------------------------------------------
+
+'''numbers = [1, 2, 3, 4, 5]
+letras = ['a' , 'b' , 'c' , 'd' , 'e']
+
+numbers.extend(letras)
+print(numbers)'''
+
+#-------------------------------------------------
+
+itens = [['item1' , 'item2'] , ['item3' , 'item4']]
+#                 0                     1
+#            0          1          0          1
+print(itens[0] [1])
---
+++ ApendendoPython/2Estrutura_de_Dados/5extraindo_variaveis_de_listas.py
@@ -0,0 +1,34 @@
+
+
+
+'''produtos = ['a', 'b', 'c', 'd']
+
+item0 = produtos[0]
+item1 = produtos[1]
+item2 = produtos[2]
+item3 = produtos[3]
+
+print (item0)
+print (item1)
+print (item2)
+print (item3)'''
+
+'''produtos = ['a', 'b', 'c', 'd']
+
+item0 , item1 , item2 , item3 = produtos
+
+print (item0)
+print (item1)
+print (item2)
+print (item3)'''
+
+'''produtos = ['a', 'b', 'c', 'd', 1 , 2 , 3, 4]
+
+item0 , item1 , item2 , *itens = produtos
+
+print (item0)
+print (item1)
+print (item2)
+print (itens)
+'''
+
