951d3f9c2d4c71437bc3d732f97c89e617d6c0cc
MarcosAredes <Ducarx40@hotmail.com>
2023-12-06 23:38:39 -0300

    Tutu

---
+++ ApendendoPython/2Estrutura_de_Dados/6looping_dentro_de_lista.py
@@ -0,0 +1,7 @@
+
+
+valores = [50, 80, 100, 120, 150]
+
+for x in valores:
+    print(f'O valor final do produto é R${x}.')
+
---
+++ ApendendoPython/2Estrutura_de_Dados/7verificando_itens_da_lista.py
@@ -0,0 +1,9 @@
+
+
+cli = input('Qual cor tu quer?')
+cores = ['amarelo' , 'vermelho' , 'azul', 'roxo']
+
+if cli.lower() in cores:
+    print('Tem sim')
+else:
+    print('Tem não ')
