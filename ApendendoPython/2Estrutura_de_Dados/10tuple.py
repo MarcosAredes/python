1db3d6d9f73d882f9d5fddd78034eb7b67fd0850
MarcosAredes <Ducarx40@hotmail.com>
2023-12-10 23:03:51 -0300

    alray

--- ApendendoPython/2Estrutura_de_Dados/10tuple.py
+++ ApendendoPython/2Estrutura_de_Dados/10tuple.py
@@ -1,9 +1,18 @@
 
 
+'''cor0 = ['amarelo', 'preto', 'azul', 'roxo']
+
+cor1 = ('amarelo', 'preto', 'azul', 'roxo')
+
+
+print(type(cor0))
+print(type(cor1))'''
+
+
 cor0 = ['amarelo', 'preto', 'azul', 'roxo']
 
 cor1 = ('amarelo', 'preto', 'azul', 'roxo')
 
 
-print(cor0)
-print(cor1)
+print(type(cor0))
+print(type(cor1))
