a24760630574baff197d361822d5c8868f3aa912
MarcosAredes <Ducarx40@hotmail.com>
2023-12-10 23:24:46 -0300

    alrray1,1,1

---
+++ ApendendoPython/2Estrutura_de_Dados/11.0arrays.py
@@ -0,0 +1,25 @@
+from array import array
+
+# Array (Matriz)
+    # Similar a lista
+    # Melhor performace
+
+letras = ['a', 'b', 'c', 'd']
+
+numeros_i = [10, 20, 30, 40]
+numeros_f = [1.2, 2.2, 3.2]
+
+print(letras)
+print(numeros_i)
+print(numeros_f)
+
+print()
+
+letras = array( 'u', ['a', 'b', 'c', 'd'])
+numeros_i = array( 'i', [10, 20, 30, 40])
+numeros_f = array ( 'f' ,  [1.2, 2.2, 3.2])
+
+
+print(letras)
+print(numeros_i)
+print(numeros_f)
