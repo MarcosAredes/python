24f1a2edb1393cf39066746cb5ca408c341b12a9
MarcosAredes <Ducarx40@hotmail.com>
2023-12-13 18:46:57 -0300

    seting

--- ApendendoPython/2Estrutura_de_Dados/12sets.py
+++ ApendendoPython/2Estrutura_de_Dados/12sets.py
@@ -7,4 +7,8 @@ list2 = [10, 20, 60, 70]
 num1 = set(list1)
 num2 = set(list2)
 
-print(num1 | num2)
+print(num1 | num2) # Union
+print(num1 - num2) # Difference
+print(num1 ^ num2) # Symmetric Difference 
+print(num1 & num2) # And
+
---
+++ ApendendoPython/2Estrutura_de_Dados/13funções_em_sets.py
@@ -0,0 +1,52 @@
+
+
+#1
+conjunto = {1, 2, 3, 4, 5}
+tamanho = len(conjunto)
+print(tamanho)  # Saída: 5
+
+#2
+conjunto = {1, 2, 3}
+conjunto.add(4)
+print(conjunto)  # Saída: {1, 2, 3, 4}
+
+#3
+conjunto = {1, 2, 3, 4}
+conjunto.remove(3)
+print(conjunto)  # Saída: {1, 2, 4}
+
+#4
+conjunto = {1, 2, 3, 4}
+conjunto.discard(3)
+print(conjunto)  # Saída: {1, 2, 4}
+
+#5
+conjunto = {1, 2, 3, 4}
+elemento_removido = conjunto.pop()
+print(elemento_removido, conjunto)
+
+#6
+conjunto1 = {1, 2, 3}
+conjunto2 = {3, 4, 5}
+uniao = conjunto1.union(conjunto2)
+print(uniao)  # Saída: {1, 2, 3, 4, 5}
+
+#7
+conjunto1 = {1, 2, 3, 4}
+conjunto2 = {3, 4, 5, 6}
+intersecao = conjunto1.intersection(conjunto2)
+print(intersecao)  # Saída: {3, 4}
+
+#8
+conjunto1 = {1, 2, 3, 4}
+conjunto2 = {3, 4, 5, 6}
+diferenca = conjunto1.difference(conjunto2)
+print(diferenca)  # Saída: {1, 2}
+
+#9
+conjunto = {1, 2, 3, 4}
+conjunto.clear()
+print(conjunto)  # Saída: set()
+
+
+
