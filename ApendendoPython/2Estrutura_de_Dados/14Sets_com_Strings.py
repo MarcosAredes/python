a2c0a0f9449832940b90df8992f9b50e86dc9a5c
Marcos <Ducarx40@hotmail.com>
2023-12-17 19:35:33 -0300

    diciionarios

---
+++ ApendendoPython/2Estrutura_de_Dados/14Sets_com_Strings.py
@@ -0,0 +1,44 @@
+# 1
+str1 = "Olá"
+str2 = " Mundo"
+
+resultado = str1 + str2
+
+print(resultado)
+
+# 2 ---------------
+
+
+str3 = "abc"
+resultado_repeticao = str3 * 3
+print(resultado_repeticao)
+
+# 3 -----------------
+texto = "Python"
+print(texto[0])
+print(texto[2])
+
+# 4 ----------------
+
+texto = "Python"
+parte = texto[1:4]
+print(parte)
+
+# 5 ----------------
+
+texto = "Python"
+comprimento = len(texto)
+print(comprimento)
+
+# 6 -----------------
+
+texto = "Python"
+resultado_verificacao = "tho" in texto
+print(resultado_verificacao)
+
+# 7 -------------------
+
+nome = "Alice"
+idade = 25
+frase = f"Olá, meu nome é {nome} e eu tenho {idade} anos."
+print(frase)
