4d646fd9f31addf3a48fcfebc06193f2569d77ae
Marcos <Ducarx40@hotmail.com>
2023-12-26 20:10:40 -0300

    class

--- ApendendoPython/4POO(progrmação_orientada_a_Objeto)/4MaisFunçõesNaClass.py
+++ ApendendoPython/4POO(progrmação_orientada_a_Objeto)/4MaisFunçõesNaClass.py
@@ -5,6 +5,9 @@ class Funcionarios:
         self.sobrenome = sobrenome
         self.nascimento = nascimento
 
+    def dados(self):
+        return self.nome + " " + self.sobrenome + " " + self.nascimento
+
 
 #   Criar o Objeto
 
@@ -13,29 +16,8 @@ usuario1 = Funcionarios("Eleanor", "Rigby", "05/08/1966")
 usuario2 = Funcionarios("mister", "Rigby", "05/08/1966")
 
 
-#   Criar os Parâmetros
-
-'''
-usuario1.nome = "Eleanor"
-usuario1.sobrenome = "Rigby"
-usuario1.nascimento = "05/08/1966"
-
-
-usuario2.nome = "mister"
-usuario2.sobrenome = "postman"
-usuario2.nascimento = "04/09/1961"'''
-
-
 #   Os dados não são passados diretamente na função, agora são associados aos parametros
 
 
 # print
-print(usuario1.nome)
-print(usuario1.sobrenome)
-print(usuario1.nascimento)
-
-print("------------")
-
-print(usuario2.nome)
-print(usuario2.sobrenome)
-print(usuario2.nascimento)
+print(usuario1.dados())
